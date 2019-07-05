a8a488b9398368bed5168849761d6e9f73db2af3
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-07-04 20:28:06 -0400

    test absolute URLs

--- precache-manifest.e78a6a4334eb9d9c6d49467143a14242.js
+++ precache-manifest.e78a6a4334eb9d9c6d49467143a14242.js
@@ -1,18 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97fa0d0fbbcdaf8256516547645f7f7e",
-    "url": "/index.html"
+    revision: "97fa0d0fbbcdaf8256516547645f7f7e",
+    url:
+      "https://raw.githubusercontent.com/brunogarciagonzalez/Energybox_wmataExplorer/gh-pages/index.html"
   },
   {
-    "revision": "bc254a11aa7ed158e263",
-    "url": "/static/js/2.61125a40.chunk.js"
+    revision: "bc254a11aa7ed158e263",
+    url:
+      "https://raw.githubusercontent.com/brunogarciagonzalez/Energybox_wmataExplorer/gh-pages/static/js/2.61125a40.chunk.js"
   },
   {
-    "revision": "cea68f20f2da3fd38950",
-    "url": "/static/js/main.5b1717c2.chunk.js"
+    revision: "cea68f20f2da3fd38950",
+    url:
+      "https://raw.githubusercontent.com/brunogarciagonzalez/Energybox_wmataExplorer/gh-pages/static/js/main.5b1717c2.chunk.js"
   },
   {
-    "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    revision: "42ac5946195a7306e2a5",
+    url:
+      "https://raw.githubusercontent.com/brunogarciagonzalez/Energybox_wmataExplorer/gh-pages/static/js/runtime~main.a8a9905a.js"
   }
 ]);
